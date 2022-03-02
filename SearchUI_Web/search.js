86d7aca444354d24ba01b73f99a461585ca7bed6
sbhatiaNasuni <sbhatia@nasuni.com>
2022-03-02 07:42:33 +0530

    Minor adjustments in code

--- SearchUI_Web/search.js
+++ SearchUI_Web/search.js
@@ -1,11 +1,6 @@
 // Update this variable to point to your domain.
-<<<<<<< HEAD
 var apigatewayendpoint = 'https://u1swlszspg.execute-api.us-east-2.amazonaws.com/dev/search-es';
 var volume_api = 'https://u1swlszspg.execute-api.us-east-2.amazonaws.com/dev/es-volume';
-=======
-var search_api = 'https://vvi2lcdn3c.execute-api.us-east-2.amazonaws.com/dev/search-es';
-var volume_api = 'https://vvi2lcdn3c.execute-api.us-east-2.amazonaws.com/dev/es-volume';
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
 var loadingdiv = $('#loading');
 var noresults = $('#noresults');
 var resultdiv = $('#results');
@@ -21,13 +16,12 @@ var numArr = []
 // var i =0;
 
 // Executes the search function 250 milliseconds after user stops typing
-searchbox.keyup(function() {
-    clearTimeout(timer);
-    timer = setTimeout(search, 500);
+searchbox.keyup(function () {
+  clearTimeout(timer);
+  timer = setTimeout(search, 500);
 });
 
 function dropDownData(period) {
-<<<<<<< HEAD
   volume = period;
   console.log(period);
 }
@@ -36,35 +30,26 @@ function paginationData(period) {
   pagiResults = period;
   console.log(pagiResults+"   pagination page number");
   indexChange();
-=======
-    volume = period;
-    console.log(period);
-    if (searchbox.val() != "") {
-        search();
-    }
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
 }
 
 async function search() {
-    // Clear results before searching
+  // Clear results before searching
+  noresults.hide();
+  resultdiv.empty();
+  loadingdiv.show();
+  // Get the query from the user
+  if (volume==undefined || volume==" "){
+    console.log("no volume selected");
+    // throw new Error("Something went badly wrong!");
+    loadingdiv.hide();
     noresults.hide();
-    resultdiv.empty();
-    loadingdiv.show();
-    // Get the query from the user
-    if (volume == undefined || volume == " ") {
-        console.log("no volume selected");
-        // throw new Error("Something went badly wrong!");
-        loadingdiv.hide();
-        noresults.hide();
-        var content = document.createElement("div");
-        content.innerHTML += "<p class='result-status'><b>No volume was selected</p>";
-        resultdiv.append(content);
-        return;
-    } else if (volume == "all") {
-        volume = "";
+    var content = document.createElement("div");
+    content.innerHTML += "<p class='result-status'><b>No volume was selected</p>";
+    resultdiv.append(content);
+    return;}else if (volume=="all"){
+      volume="";
     }
 
-<<<<<<< HEAD
   let query = searchbox.val()+"~"+volume;
   console.log(query);
   // Only run a query if the string contains at least three characters
@@ -87,65 +72,42 @@ async function search() {
   }
 }
   loadingdiv.hide();
-=======
-    let query = searchbox.val() + "~" + volume;
-    console.log(query);
-    // Only run a query if the string contains at least three characters
-    if (query.length > 0) {
-        // Make the HTTP request with the query as a parameter and wait for the JSON results
-        let response_data = await $.get(search_api, { q: query, size: 25 }, 'json');
-        console.log(response_data);
-        //console.log(response,'response...');
-        // Get the part of the JSON response that we care about
-        //let results = response['hits']['hits'];
-        if (response_data.length > 0) {
-            loadingdiv.hide();
-            noresults.hide();
-            // Iterate through the results and write them to HTML
-            resultdiv.append('<p class="result-status">Found ' + response_data.length + ' results.</p>');
-            appendData(resultdiv, response_data);
-        } else {
-            noresults.show();
-        }
-    }
-    loadingdiv.hide();
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
 }
 
 //Iterate volume names from API to drop down menu
 async function start() {
-    response = await $.get(volume_api, 'json');
-    arr = response.split(",");
-    var chars = ['[', ']', '\\', '"'];
-    replaceAll(chars);
+  response = await $.get(volume_api,'json');
+  arr = response.split(",");
+  var chars = ['[',']','\\','"'];
+  replaceAll(chars);
 }
 
 //Filtering and removing extra characters
 function replaceAll(chars) {
-    for (var i = 0; i < chars.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            var x = String(arr[j])
+  for ( var i=0;i<chars.length;i++) {
+      for (var j=0;j<arr.length;j++) {
+          var x = String(arr[j])
 
-            x = x.replaceAll(chars[i], '');
-            x = x.replaceAll(/\s/g, '')
-            arr[j] = x;
-        }
-
-    }
-    console.log(arr);
-    appendDropDown(arr);
+          x = x.replaceAll(chars[i],'');
+          x = x.replaceAll(/\s/g,'')
+          arr[j]=x;
+      }
+      
+  }
+  console.log(arr);
+  appendDropDown(arr);
 }
 
 //Appending from volume name array to drop down
 function appendDropDown(arr) {
-    var select = document.getElementById("selectVolume");
-    for (var i = 0; i < arr.length; i++) {
-        var opt = arr[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-    }
+  var select = document.getElementById("selectVolume");
+  for(var i = 0; i < arr.length; i++) {
+    var opt = arr[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
 
 }
 
@@ -156,7 +118,11 @@ function indexChange(){
     numArr.push(y);
   }
   numArr= [...new Set(numArr)]
+  // console.log()
   index = numArr[pagiResults-1]
+  
+  // resultdiv.append('<p class="result-status">Found ' + responseArr.length + ' results.</p>');
+  console.log(numArr+" this is new numArr");
   noresults.hide();
   resultdiv.empty();
   loadingdiv.hide();
@@ -166,20 +132,14 @@ function indexChange(){
 
 //Appending all the results to the main resultdiv 
 function appendData(resultdiv, data) {
-<<<<<<< HEAD
   console.log("dataLen" + dataLen)
     for (var i=index; i < dataLen+index; i++) {
-=======
-    var count;
-    for (var i = 0; i < data.length; i++) {
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
         var link = document.createElement("h5");
         var content = document.createElement("span");
         var resultBox = document.createElement("div");
         var spanDiv = document.createElement('div');
         resultBox.classList.add("result-box");
         spanDiv.classList.add("result-content");
-<<<<<<< HEAD
 		
 		if (data[i].length > 0) {
       
@@ -196,32 +156,16 @@ function appendData(resultdiv, data) {
         resultdiv.append(resultBox);
         
 				}
-=======
-
-        if (data[i].length > 0) {
-
-            for (var j = 0; j < data[i].length; j++) {
-                link.innerHTML = "<a class='elasti_link result-title' href=" + data[i][j]._source.access_url + ">" + data[i][j]._source.object_key + "</a>" + "<br>";
-                resultBox.append(link);
-                if (data[i][j].highlight.content.length > 0) {
-                    for (var k = 0; k < data[i][j].highlight.content.length; k++) {
-                        content.innerHTML += data[i][j].highlight.content[k];
-                        spanDiv.append(content);
-                        resultBox.append(spanDiv);
-                    }
-                    resultdiv.append(resultBox);
-                }
-            }
-
-            stop();
-
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
         }
-
+        
+        stop();
+			
+		}
+    
     }
-<<<<<<< HEAD
     if(data.length>1){
       var totalPages=Math.round(data.length/dataLen);
+      console.log(totalPages+"=============")
       var page =1
       var paginationDiv = document.getElementById('pagination');
         var ul = document.createElement('ul')
@@ -229,6 +173,3 @@ function appendData(resultdiv, data) {
         createPagination(totalPages, page);
     }
 }
-=======
-}
->>>>>>> 797b42fac32ea510e33d5171be9cfc929a904a9a
