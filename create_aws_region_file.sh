0a3b9978148b7597a7a090e73b27778de9c4ecec
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-05-30 02:41:27 -0700

    Update create_aws_region_file.sh
    user_vpc_id

--- create_aws_region_file.sh
+++ create_aws_region_file.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 AWS_REGION="$1"
 USER_SUBNET_ID="$2"
-UI_TFVARS_FILE="$3"
+USER_VPC_ID="$3"
+UI_TFVARS_FILE="$4"
 echo "region="\"$AWS_REGION\" >$UI_TFVARS_FILE
 echo "user_subnet_id="\"$USER_SUBNET_ID\" >> $UI_TFVARS_FILE
+echo "user_vpc_id="\"$USER_VPC_ID\" >> $UI_TFVARS_FILE
