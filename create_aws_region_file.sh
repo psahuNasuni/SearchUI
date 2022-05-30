972ceaac744ffa77fa749d6f999eaa2e926ab5b1
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-05-30 01:19:52 -0700

    Update create_aws_region_file.sh
    Adding user_subnet_id

--- create_aws_region_file.sh
+++ create_aws_region_file.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 AWS_REGION="$1"
-UI_TFVARS_FILE="$2"
+USER_SUBNET_ID="$2"
+UI_TFVARS_FILE="$3"
 echo "region="\"$AWS_REGION\" >$UI_TFVARS_FILE
+echo "user_subnet_id="\"$USER_SUBNET_ID\" >> $UI_TFVARS_FILE
