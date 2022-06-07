01b0305415fc8a801c802e94db3b40a602cfa47d
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-06-07 02:03:57 -0700

    Update create_aws_region_file.sh
    Change for $USE_PRIVATE_IP

--- create_aws_region_file.sh
+++ create_aws_region_file.sh
@@ -2,8 +2,10 @@
 AWS_REGION="$1"
 USER_SUBNET_ID="$2"
 USER_VPC_ID="$3"
-UI_TFVARS_FILE="$4"
+USE_PRIVATE_IP="$4"
+UI_TFVARS_FILE="$5"
 echo "region="\"$AWS_REGION\" >$UI_TFVARS_FILE
 echo "user_subnet_id="\"$USER_SUBNET_ID\" >> $UI_TFVARS_FILE
 echo "user_vpc_id="\"$USER_VPC_ID\" >> $UI_TFVARS_FILE
+echo "use_private_ip="\"$USE_PRIVATE_IP\" >> $UI_TFVARS_FILE
 echo "" >> $UI_TFVARS_FILE
