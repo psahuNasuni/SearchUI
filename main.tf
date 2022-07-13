570807f7391a5c0d216118a80a6523e41800f254
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-07-13 11:04:01 -0700

    Update main.tf
    private_dns_enabled = false for testing

--- main.tf
+++ main.tf
@@ -333,7 +333,7 @@ resource "aws_vpc_endpoint" "SearchES-API-vpc-endpoint" {
   vpc_id              = var.user_vpc_id
   service_name        = data.aws_vpc_endpoint_service.vpc-endpoint-service.service_name
   vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+  private_dns_enabled = false
   security_group_ids  = [data.aws_security_groups.es.ids[0]]
   subnet_ids          = [var.user_subnet_id]
 }
