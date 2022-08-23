bf78304e4ff91eebf7e18d48cb083ed9a4b74092
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-08-22 23:17:11 -0700

    Update create_aws_region_file.sh
    addnig new parameter nac_scheduler_name

--- create_aws_region_file.sh
+++ create_aws_region_file.sh
@@ -6,6 +6,7 @@ USE_PRIVATE_IP="$4"
 UI_TFVARS_FILE="$5"
 AWS_PROFILE="$6"
 SECURITY_GROUP_ID="$7"
+NAC_SCHEDULER_NAME="$8"
 SERVICE_NAME="com.amazonaws.$AWS_REGION.execute-api"
 # Get VPC Endpoint using AWS CLI command
 VPC_ENDPOINT_ID=`aws ec2 describe-vpc-endpoints --profile $AWS_PROFILE --region $AWS_REGION | jq -r '.VpcEndpoints[]|select(.VpcId == '\"$USER_VPC_ID\"' and .ServiceName=='\"$SERVICE_NAME\"') | {VpcEndpointId}' | jq -r '.VpcEndpointId'`
@@ -16,4 +17,5 @@ echo "user_vpc_id="\"$USER_VPC_ID\" >> $UI_TFVARS_FILE
 echo "vpc_endpoint_id="\"$VPC_ENDPOINT_ID\" >> $UI_TFVARS_FILE
 echo "use_private_ip="\"$USE_PRIVATE_IP\" >> $UI_TFVARS_FILE
 echo "nac_es_securitygroup_id="\"$SECURITY_GROUP_ID\" >> $UI_TFVARS_FILE
+echo "nac_scheduler_name="\"$NAC_SCHEDULER_NAME\" >> $UI_TFVARS_FILE
 echo "" >> $UI_TFVARS_FILE
