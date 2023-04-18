04ea033f9133718b0fd752d7016efe62f9b98f27
sbhatiaNasuni <87188108+sbhatiaNasuni@users.noreply.github.com>
2023-04-18 18:28:29 +0530

    Minor change for OS and ES option of NAC provision

--- Tracker_UI/docs/fetch.js
+++ Tracker_UI/docs/fetch.js
@@ -5,7 +5,7 @@ var services = [];
 var rowIndex = 0;
 var source;
 var serviceName
-var serviceList = ["ES","KENDRA"]
+var serviceList = ["ES","OS","KENDRA"]
 var schedulerName = "kendra-tracker-ui"
 var trackerDoc
 
@@ -74,9 +74,9 @@ function tableAppend(result,volumes) {
                     button.appendChild(a)
                     a.appendChild(link)
 
-                    if(source[i]._source.service==="KENDRA"){
+                    if(source[i]._source.service==="KENDRA" || source[i]._source.service==="kendra"){
                         a.href=source[i]._source.kendra_url
-                    }else if(source[i]._source.service==="ES"){
+                    }else if(source[i]._source.service==="ES" || source[i]._source.service==="OS"){
                         a.href=source[i]._source.default_url+"?q="+source[i]._source.volume
                     }
                 
